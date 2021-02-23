a0c79f9d25363a7d5bac6de60696d780f8d624a1
Christina2020 <46932799+Christina2020@users.noreply.github.com>
2021-02-23 14:54:51 -0500

    Class assignment 2/23/2021

---
+++ 2_23_inclass_practes .R
@@ -0,0 +1,22 @@
+
+#What research question do you want to answer?
+#Does the school year make people with mental illness twitter more about mental 
+#health awareness than people without 
+  
+#What are two possible answers?
+# the first possible outcome is that they will 
+# the sceond possible outcome is the there will be no chage in twitter
+  
+#What are the documents of text data you will analyze?
+
+# people who self identfy as having a mental illness
+# poeple who does not have a mental illness
+  
+  
+  
+#Give two examples of documents in the corpus that you expect will be high in similarity to each other? Two examples that are *low* in similarity?
+  
+#https://www.nature.com/articles/s41598-017-12961-9
+# during the school year I expect the word awerness will be heih and  Profanity will be low
+  
+  
